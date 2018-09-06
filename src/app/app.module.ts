4beb5f74cf53c8c251ba1862d76b7babafb7a560
santosh <santosh.ece06@gmail.com>
2018-09-06 21:35:08 +0530

    flex-layout module added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 
 import { AppComponent } from './app.component';
 import { appRoutes, appComponents} from './app-routes';
@@ -14,6 +16,7 @@ import { SafePipe } from './common/safe.pipe';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
