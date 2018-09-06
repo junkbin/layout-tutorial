9a189bbcffaef9706b93935d37ea6db5f6a5a33a
santosh <santosh.ece06@gmail.com>
2018-09-06 22:36:29 +0530

    flex layout.

--- src/app/page7/page7.component.ts
+++ src/app/page7/page7.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page7Component implements OnInit {
 
+  data:any;
+
   constructor() { }
 
   ngOnInit() {
+    this.data = {
+      '1' : 1,
+      '2' : 2,
+      '3' : 3,
+      '4' : 4,
+      '5' : 5,
+      '6' : 6,
+      '7' : 6,
+      '8' : 6,
+      '9' : 6,
+      '10' : 6,
+      '11' : 6,
+      '12' : 6,
+    };
   }
 
 }
