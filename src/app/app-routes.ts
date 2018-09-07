280d3da560f06502462363d169fe020093a866f2
santosh <santosh.ece06@gmail.com>
2018-09-07 10:43:07 +0530

    page 8 added.

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -6,6 +6,8 @@ import { Page4Component } from './page4/page4.component';
 import { Page5Component } from './page5/page5.component';
 import { Page6Component } from './page6/page6.component';
 import { Page7Component } from './page7/page7.component';
+import { Page8Component } from './page8/page8.component';
+
 
 export const appRoutes: Routes = [
     {path: 'page1', component: Page1Component},
@@ -15,10 +17,11 @@ export const appRoutes: Routes = [
     {path: 'page5', component: Page5Component},
     {path: 'page6', component: Page6Component},
     {path: 'page7', component: Page7Component},
+    {path: 'page8', component: Page8Component},
     {path: '', redirectTo: '/page1', pathMatch: 'full'}
 ];
 
 export const appComponents = [
                     Page1Component, Page2Component, Page3Component, Page4Component,
-                    Page5Component, Page6Component, Page7Component
+                    Page5Component, Page6Component, Page7Component, Page8Component
                 ];
